5f972a7a54d42e9df21908c739752347ba241891
Erlantzcg89 <erlantzcg89@gmail.com>
2020-01-28 11:13:15 +0100

    programacion funcional arrays

--- src/app/paginas/arrays/arrays.component.ts
+++ src/app/paginas/arrays/arrays.component.ts
@@ -14,8 +14,12 @@ export class ArraysComponent implements OnInit {
   frutas: any;
 
   total: Number;
-
-
+  nombresFrutas: any;
+  totalDescuento: Number;
+  totalAmarillas: Number;
+  frutasAmarillas: any;
+  frutasVerdes: any;
+  colores: any;
 
   constructor(private pokemonService: PokemonService) {
 
@@ -26,12 +30,10 @@ export class ArraysComponent implements OnInit {
     this.total = 0;
 
     this.frutas = [
-      { 'nombre': 'fresa', 'precio': 2.45 },
-      { 'nombre': 'pera', 'precio': 3.5 },
-      { 'nombre': 'manzana', 'precio': 1.99 }
+      { 'nombre': 'fresa', 'precio': 2.45, 'descuento': 0, 'colores': ['rojo'] },
+      { 'nombre': 'pera', 'precio': 3.5, 'descuento': 10, 'colores': ['amarillo', 'verde'] },
+      { 'nombre': 'manzana', 'precio': 1.99, 'descuento': 50, 'colores': ['amarillo', 'verde', 'rojo'] }
     ];
-
-
 
   }// constructor
 
@@ -48,6 +50,47 @@ export class ArraysComponent implements OnInit {
         console.trace('petición completa');
       }
     );
+
+    // operaciones con arrays
+    this.nombresFrutas = [];
+    this.frutasAmarillas = [];
+    this.frutasVerdes = [];
+    this.colores = [];
+    this.total = 0;
+    this.totalDescuento = 0;
+    this.totalAmarillas = 0;
+
+    // this.frutas.forEach(f => {
+    //   //  this.total += f.precio;
+    //   this.nombresFrutas.push(f.nombre);
+    // });
+
+    this.nombresFrutas = this.frutas.map(el => el.nombre);
+
+    // this.total = this.frutas.reduce((previous, current) => {
+    //   console.debug(previous, current);
+    //   return previous + current.precio;
+    // }, 0);
+
+    // this.total = this.frutas.map(elemento => {
+    //   return elemento.precio;
+    // }).reduce((previous, current) => {
+    //   return previous + current;
+    // }, 0);
+
+    this.total = this.frutas.map(el => el.precio).reduce((p, c) => p + c);
+    this.nombresFrutas.push(this.frutas.map(el => el.nombre).reduce((p, c) => p + c));
+
+    this.totalDescuento = this.frutas.filter(fruta => fruta.descuento > 0).reduce((p, c) => (p + c.precio), 0);
+
+    this.frutasAmarillas = this.frutas.filter(fruta => fruta.colores.includes('amarillo'));
+    this.totalAmarillas = this.frutasAmarillas.map(el => el.precio).reduce((p, c) => p + c);
+
+    this.frutasVerdes = this.frutas.filter(fruta => fruta.colores.includes('verde'));
+
+    this.colores = this.frutas.reduce((p, c) => p.concat(c.colores), []);
+    this.colores = new Set(this.colores);
+
 
   }// ngOnInit
 
